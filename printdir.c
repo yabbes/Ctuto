7db829d79df2477c3ca7461980077a88c9ad5c40
yabbes <yabbaspost@gmail.com>
2016-06-25 12:11:18 +0200

    now accepts argv to print any directory

--- printdir.c
+++ printdir.c
@@ -1,14 +1,33 @@
+// 
+// prints directory in argv[1] with file name,
+// ino number and length
+//
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main (int argc, char *argv[])
 {
 	DIR *dir;
 	struct dirent *sd;
-
-	dir = opendir(".");
+	char path[50];
+	
+	// if argv[1] is empty set . current directory
+	if (argv[1] == NULL)
+	{ 
+		printf("no argv was given"
+				"printing contents of current directory");
+		sprintf(path, ".");
+	}else{
+		//set path with first argv
+		strcpy(path, argv[1]);
+		printf("printing contents of dir %s",
+					path);
+	}
+	dir = opendir(path);
 
 	if (dir == NULL) 
 	{
