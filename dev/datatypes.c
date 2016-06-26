f344e32b10b3d8bccb4486fca5298c26f1f3b002
yabbes <yabbaspost@gmail.com>
2016-06-26 13:49:23 +0200

    datatypes2

---
+++ dev/datatypes.c
@@ -0,0 +1,18 @@
+//
+// just printing the size of some datatypes
+//
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+	float j;
+	short k;
+	long m;
+
+	printf("size of int: \t %d\n", sizeof(int));
+	printf("size of float: \t %d\n", sizeof(float));
+	printf("size of short: \t %d\n", sizeof(short));
+	printf("size of long: \t %d\n", sizeof(long));
+
+}
