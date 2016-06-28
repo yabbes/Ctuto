e28c035542a67a748233bf6c0fb69bbebe7f8c2e
yabbes <yabbaspost@gmail.com>
2016-06-28 20:55:58 +0200

    fix simple interest

--- dev/interest.c
+++ dev/interest.c
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
 {
 	float p, t; // principal amount and time
 	float r, si; // rate and simple interest
+	float amt; // principal amount plus simple interest
 
 	printf("Principal interest calculator\n"); 
 	printf("Please enter principal amount\n"); 
@@ -21,9 +22,11 @@ int main(int argc, char *argv[])
 
 	// Calculations :>
 	si = (p*r*t)/100;
+	amt = si + p;
 	// debugging line
 	// printf("%f, %f, %f\n", p, r, t);
+	printf("\np: %.2f\tr: %.2f\tt: %.0f\n", p, r, t);
 	printf("\nCalculated simple interest: \t %.2f\n", si);
-
+	printf("This amounts to a total of: %.2f\n", amt);
 	return 0;
 }
